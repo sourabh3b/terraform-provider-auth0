249c530e10e2429c8b215575d77618bb1404b6f2
Sourabh Bhagat <sbhagat@aerospike.com>
2022-11-03 13:24:34 -0700

    auth0 terraform poc

--- examples/multiple-environments/stage/main.tf
+++ examples/multiple-environments/stage/main.tf
@@ -1,7 +1,19 @@
 provider "auth0" {
-  domain = "example-stage.us.auth0.com"
+  domain = "aerospike-dev.us.auth0.com"
 }
 
 module "admin_console" {
   source = "../modules/admin_console"
+}
+
+resource "auth0_client" "my_client" {
+  name            = "Aerospike Cloud"
+  description     = "Aerospike Cloud Application"
+  app_type        = "spa"
+  callbacks       = ["http://localhost:3000/callback", "http://localhost:3000", "https://laughing-goggles-219a4c7f.pages.github.io/", "https://legendary-bassoon-0465702a.pages.github.io/", "http://insomnia", "https://oauth.pstmn.io/v1/callback"]
+  oidc_conformant = true
+
+  jwt_configuration {
+    alg = "RS256"
+  }
 }
--- examples/multiple-environments/stage/versions.tf
+++ examples/multiple-environments/stage/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     auth0 = {
       source = "auth0/auth0"
+      version = "~> 0.34.0"
     }
   }
 }
